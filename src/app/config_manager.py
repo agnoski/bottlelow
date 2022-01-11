6ee431b3613a2be2e2c92ecc66ae47ce3c999a3a
Francesco Agnolazza <agnolazf@amazon.com>
2022-01-11 23:40:11 +0100

    Add logging in ConfigManager

--- src/app/config_manager.py
+++ src/app/config_manager.py
@@ -3,14 +3,15 @@ import yaml
 
 class ConfigManager:
     def __init__(self, path):
-        logging.getLogger(self.__class__.__name__)
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.path = path
         with open(self.path, "r") as file:
             try:
                 self.config = yaml.safe_load(file)
             except yaml.YAMLError as exception:
-                logging.error(exception)
+                self.logger.error(exception)
 
     def save(self):
         with open(self.path, "w") as file:
+            self.logger.info("Saving config...")
             yaml.dump(self.config, file)
